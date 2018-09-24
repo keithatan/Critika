f1e478864bf636e3ec4ef2ba80e7faad44b353ee
Cyrus Santiago <santiac@purdue.edu>
2018-09-24 11:44:05 -0500

    added button to add friends from profile page

--- frontend/critika/src/Profile.js
+++ frontend/critika/src/Profile.js
@@ -7,7 +7,7 @@ class Profile extends React.Component{
   render(){
   return (
     <div>
-      <h1>User Profile  </h1>
+      <h1>User Profile <Button type="primary" size="large">+ Add Friend</Button> </h1>
       
       <div style={{ background: '#ECECEC', padding: '30px' }}>
     <Row gutter={16}>
