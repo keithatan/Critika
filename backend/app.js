3022ee1cc87ff2b959c4231793168762946f0eca
Keith <tan230@purdue.edu>
2018-09-23 20:48:40 -0400

    Log application start

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,10 @@ const express = require ('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-
+app.get('/', (res, req) => {
     res.send('Critika Application is created');
 });
-app.listen(3000);
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000')
+});
