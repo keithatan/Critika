e8f5245ca7d9ed089928992b57633f75614d1077
Cyrus Santiago <santiac@purdue.edu>
2018-09-24 11:01:46 -0500

    added header and side bar

--- frontend/critika/src/App.js
+++ frontend/critika/src/App.js
@@ -11,76 +11,83 @@ import Community from './Community';
 import Explore from './Explore';
 
 import './App.css';
-import { Button } from 'antd';
+import { Button, Layout, Menu, Breadcrumb, Icon } from 'antd';
 import 'antd/dist/antd.css';
 import { Link, Switch, Route, Redirect } from 'react-router-dom'
+
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <div Button>
-          <Link to="/login">Login</Link>
-        </div>
-        <div Button>
-          <Link to="/profile">Profile</Link>
-        </div>
-        <div Button>
-          <Link to="/groups">Groups</Link>
-        </div>
-        <div Button>
-          <Link to="/submissions">Submissions</Link>
-        </div>
-        <div Button>
-          <Link to="/feedbackGiven">Feedback Given</Link>
-        </div>
-        <div Button>
-          <Link to="/messages">Messages</Link>
-        </div>
-        <div Button>
-          <Link to="/friends">Friends</Link>
-        </div>
-        <div Button>
-          <Link to="/community">Community</Link>
-        </div>
-        <div Button>
-          <Link to="/explore">Explore</Link>
-        </div>
-      <Switch>
       
-        <Route path='/login' render = { () => (
-          <Login/>)} 
-        />
-        <Route path='/profile' render = { () => (
-          <Profile/>)} 
-        />
-        <Route path='/groups' render = { () => (
-          <Groups/>)} 
-        />
-        <Route path='/submissions' render = { () => (
-          <Submissions/>)} 
-        />
-        <Route path='/feedbackGiven' render = { () => (
-          <FeedbackGiven/>)} 
-        />
-        <Route path='/messages' render = { () => (
-          <Messages/>)} 
-        />
-        <Route path='/friends' render = { () => (
-          <Friends/>)} 
-        />
-        <Route path='/community' render = { () => (
-          <Community/>)} 
-        />
-        <Route path='/explore' render = { () => (
-          <Explore/>)} 
-        />
-         
-      </Switch>
+      <div>
+       <Layout>
+    <Header>
+      
+      <header className="App-header">CRITIKA</header>
+    </Header>
+    <Layout>
+      <Sider width={200} style={{ background: '#fff' }}>
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          style={{ height: '100%', borderRight: 0 }}
+        >
+            <Menu.Item key="1"><Link to="/login">Login</Link></Menu.Item>
+            <Menu.Item key="2"><Link to="/community">Community</Link></Menu.Item>
+            <Menu.Item key="3"><Link to="/explore">Explore</Link></Menu.Item>
+            <Menu.Item key="5"><Link to="/profile">Profile</Link></Menu.Item>
+            <Menu.Item key="6"><Link to="/groups">Groups</Link></Menu.Item>
+            <Menu.Item key="7"><Link to="/messages">Messages</Link></Menu.Item>
+            <Menu.Item key="8"><Link to="/submissions">Submissions</Link></Menu.Item>
+            <Menu.Item key="8"><Link to="/feedbackGiven">FeedbackGiven</Link></Menu.Item>
+
+        
+        </Menu>
+      </Sider>
+      <Layout style={{ padding: '0 24px 24px' }}>
+        
+        <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
+        <Switch>
+      
+      <Route path='/login' render = { () => (
+        <Login/>)} 
+      />
+      <Route path='/profile' render = { () => (
+        <Profile/>)} 
+      />
+      <Route path='/groups' render = { () => (
+        <Groups/>)} 
+      />
+      <Route path='/submissions' render = { () => (
+        <Submissions/>)} 
+      />
+      <Route path='/feedbackGiven' render = { () => (
+        <FeedbackGiven/>)} 
+      />
+      <Route path='/messages' render = { () => (
+        <Messages/>)} 
+      />
+      <Route path='/friends' render = { () => (
+        <Friends/>)} 
+      />
+      <Route path='/community' render = { () => (
+        <Community/>)} 
+      />
+      <Route path='/explore' render = { () => (
+        <Explore/>)} 
+      />
+       
+    </Switch>
+        </Content>
+      </Layout>
+    </Layout>
+  </Layout>
+       
+      
       </div>
     
       
