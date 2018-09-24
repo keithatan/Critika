c4926b05de73f905b1fbd229ada604f39bfe5695
Keith <tan230@purdue.edu>
2018-09-24 00:39:59 -0400

    Created register route for users

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ var submission = require('./routes/submissions.js');
 const app = express();
 
 app.get('/', (res, req) => {
-    res.send('Critika Application is created');
+    res.send('API start');
 });
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000')
 });
 
+/* Routes */
 app.use('/user', user);
 app.use('/submission', submission);
 
