e2edd9f6689e865237bda04a4b652af2bfdaab7c
Cyrus Santiago <santiac@purdue.edu>
2018-09-24 11:55:46 -0500

    added login forms

--- frontend/critika/src/Login.js
+++ frontend/critika/src/Login.js
@@ -1,14 +1,41 @@
 import React from 'react'
 import './App.css'
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+
+const FormItem = Form.Item;
 
 class Login extends React.Component{
-  render(){
-  return (
-    <div>
-      <p>Please login to view site content!</p>
-    </div>
-  );
+  
+
+  render() {
+    return (
+      <Form className="login-form">
+        <FormItem>
+          
+            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)'}} />} placeholder="Username" />
+          
+        </FormItem>
+        <FormItem>
+          
+            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)'}} />} type="password" placeholder="Password" />
+          
+        </FormItem>
+        <FormItem>
+          
+            <Checkbox>Remember me</Checkbox>
+          
+          <a className="login-form-forgot" href="">Forgot password</a>
+          <p>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+          Or <a href="">register now!</a>
+          </p>
+        </FormItem>
+      </Form>
+    );
   }
-} 
+}
+
 
 export default Login
