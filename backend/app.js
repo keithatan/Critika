9b67e6cab82e6307db260a3bbce7141ccbda5f4f
keithatan <tan230@purdue.edu>
2018-09-25 17:41:55 -0400

    Added dotenv
    Allows us to save environment variables to a safe spot

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,36 @@
 const express = require ('express');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+let cookieParser = require('cookie-parser');
+
+require('dotenv').config();
+
+
 
 /* Routes */
-var user = require('./routes/user.js');
-var submission = require('./routes/submissions.js');
+let user = require('./routes/user.js');
+let submission = require('./routes/submissions.js');
 
 const config = require('./config/config');
 
 const app = express();
 
-app.get('/', (res, req) => {
-    res.send('API start');
-});
 
-app.listen(3000, () => {
-    console.log('The application is running on localhost:3000')
-});
 
 /* Parsers */
 app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 
 /* Routes */
 app.use('/user', user);
 app.use('/submission', submission);
+
+app.get('/', (res, req) => {
+    console.log(process.env.MONGODB_HOST);
+});
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000')
+});
 
 module.exports = app;
