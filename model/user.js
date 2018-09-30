06ab7d3f9301170187e782722ab9fe3a27c181b3
keithatan <tan230@purdue.edu>
2018-09-30 18:51:59 -0400

    Added email validation for emails

--- model/user.js
+++ model/user.js
@@ -3,14 +3,22 @@ const mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
+const vdator = require('validator');
 
 /* Make the schema */
 let userSchema = new Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  username: { type: String, required: true, unique: true, minlength: 1, trim: true },
+  password: { type: String, required: true, minlength: 6 },
   verified: Boolean,
-  email:  String,
+  email:{  
+    type: String,
+    validate: {
+      validator: vdator.isEmail(value),
+      message: '{VALUE} is not a valid email'
+    }
+  },
   security_question: String,
+  
 });
 
 /* Creating the user model from the schema and giving it to Mongoose */
