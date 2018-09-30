86e4c95e3fc8b0757350d6fbfb9b18bb46897a83
julzsd12 <julzsd12@gmail.com>
2018-09-30 16:48:31 -0400

    created a button to redirect user from Dashboard to Profile

--- frontend/critika/src/Dashboard.js
+++ frontend/critika/src/Dashboard.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import './App.css'
+import {Button} from 'antd'
+import {Link} from 'react-router-dom'
 
 class Dashboard extends React.Component{
   render(){
   return (
     <div>
       <h1>Dashboard</h1>
+      <Button>
+      <Link to="/profile">Profile</Link>
+      </Button>
+      
     </div>
   );
   }
