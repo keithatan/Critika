1dabe8646c2c29e58994f5187f175f504ff38b5e
Cyrus Santiago <santiac@purdue.edu>
2018-09-30 20:19:14 -0500

    added UI components for admin features

--- frontend/critika/src/Submissions.js
+++ frontend/critika/src/Submissions.js
@@ -64,7 +64,7 @@ class Submissions extends React.Component{
       <Button type="primary">Remove Submission</Button>
       <Button type="primary">Edit Submission</Button>
 
-      <Carousel afterChange={onChange}>
+      <Carousel afterChange={onChange} style={{backgroundColor: 'rgb(194, 165, 211)'}}>
         <div><h3>1</h3></div>
         <div><h3>2</h3></div>
         <div><h3>3</h3></div>
