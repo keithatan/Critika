4d07863c848fb663fe09b50c7f18abc213934415
Connor Todd <todd37@purdue.edu>
2018-10-02 16:36:32 -0400

    Organize unit tests, add files for post

--- backend/tests/test_get_submission.js
+++ backend/tests/test_get_submission.js
@@ -10,42 +10,7 @@ var testAccountToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YmIzYjY
 
 chai.use(chaiHttp);
 
-describe('Test GET', function(){
-
-  describe('Test GET /user/account', function(){
-
-    it('GET request without auth token should return 401', function(done){
-      chai.request(server).get('/user/account').end(function(err, res){
-        res.should.have.status(401);
-        done();
-      });
-    });
-
-    it('GET request without correct auth token should return 401', function(done){
-      chai.request(server).get('/user/account').set('x-auth', 'a bad key').end(function(err, res){
-        res.should.have.status(401);
-        done();
-      });
-    });
-
-    it('GET requset with correct auth token should return 200', function(done){
-      chai.request(server).get('/user/account').set('x-auth', testAccountToken).end(function(err, res){
-        res.should.have.status(200);
-        done();
-      });
-    });
-
-    it('GET request should return an object with id, username, and email', function(done){
-      chai.request(server).get('/user/account').set('x-auth', testAccountToken).end(function(err, res){
-        res.should.be.json;
-        res.body.should.be.a('object');
-        res.body.should.have.property('_id');
-        res.body.should.have.property('username');
-        res.body.should.have.property('email');
-        done();
-      });
-    })
-  });
+describe('Test GET Submission', function(){
 
   describe('Test GET /submission/mine', function(){
 
@@ -70,8 +35,7 @@ describe('Test GET', function(){
       });
     });
 
-    it('GET request should return all user submissions',
-     function(done){
+    it('GET request should return all user submissions with proper response', function(done){
       chai.request(server).get('/submission/mine').set('x-auth', testAccountToken).end(function(err, res){
         res.should.be.json;
         res.body.should.be.a('array');
---
+++ backend/tests/test_get_user.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../app');
+var should = chai.should();
+
+/* USAGE mocha /tests/<file> */
+
+var testAccountToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YmIzYjY2MGQ5MWZjMDNhZDg1OTFmODIiLCJhY2Nlc3MiOiJhdXRoIiwiaWF0IjoxNTM4NTA0Mzg0fQ.ivTO6L-Cmfe2y469aQUVDamlcgxmzuiInyV4ya5LAOY'
+
+chai.use(chaiHttp);
+
+describe('Test GET user', function(){
+
+    describe('Test GET /user/account', function(){
+
+        it('GET request without auth token should return 401', function(done){
+          chai.request(server).get('/user/account').end(function(err, res){
+            res.should.have.status(401);
+            done();
+          });
+        });
+    
+        it('GET request without correct auth token should return 401', function(done){
+          chai.request(server).get('/user/account').set('x-auth', 'a bad key').end(function(err, res){
+            res.should.have.status(401);
+            done();
+          });
+        });
+    
+        it('GET requset with correct auth token should return 200', function(done){
+          chai.request(server).get('/user/account').set('x-auth', testAccountToken).end(function(err, res){
+            res.should.have.status(200);
+            done();
+          });
+        });
+    
+        it('GET request should return an object with id, username, and email', function(done){
+          chai.request(server).get('/user/account').set('x-auth', testAccountToken).end(function(err, res){
+            res.should.be.json;
+            res.body.should.be.a('object');
+            res.body.should.have.property('_id');
+            res.body.should.have.property('username');
+            res.body.should.have.property('email');
+            done();
+          });
+        })
+        
+     });
+})
