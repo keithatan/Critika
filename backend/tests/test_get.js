28b00ad9dffa8d69fb4b5bbe68d4347927991756
Connor Todd <todd37@purdue.edu>
2018-10-02 14:56:52 -0400

    Mocha USAGE

--- backend/tests/test_get.js
+++ backend/tests/test_get.js
@@ -4,6 +4,8 @@ var chaiHttp = require('chai-http');
 var server = require('../app');
 var should = chai.should();
 
+/* USAGE mocha /tests/<file> */
+
 var testAccountToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YmIzYjY2MGQ5MWZjMDNhZDg1OTFmODIiLCJhY2Nlc3MiOiJhdXRoIiwiaWF0IjoxNTM4NTA0Mzg0fQ.ivTO6L-Cmfe2y469aQUVDamlcgxmzuiInyV4ya5LAOY'
 
 chai.use(chaiHttp);
