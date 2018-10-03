866c9d9fcc2e1d541ee916baadd9d2a9d1b210e4
kcsodetz <kensodetz@gmail.com>
2018-10-02 22:42:49 -0400

    Add remove submission route

--- routes/submission.js
+++ routes/submission.js
@@ -12,14 +12,17 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 /* Objects */
 var Submission = require('../model/submission');
 
-router.get("/",  (req, res) => {
+router.get("/", (req, res) => {
     res.send('This route is for all submission related tasks');
 });
 
+/**
+ * Adds submission
+ */
 router.post("/add", authenticate, (req, res) => {
 
-    if(!req.body.category || !req.body.submissionName || !req.body.submissionText){
-        res.status(400).json({message: "Submission data is incomplete"});
+    if (!req.body.category || !req.body.submissionName || !req.body.submissionText) {
+        res.status(400).json({ message: "Submission data is incomplete" });
         return;
     }
 
@@ -32,28 +35,46 @@ router.post("/add", authenticate, (req, res) => {
     });
 
     /* Add to database */
-    newSubmission.save().then(() =>{
+    newSubmission.save().then(() => {
         res.status(200).send(newSubmission);
     }).catch((err) => {
         res.status(400).send(err);
     })
 })
 
+/**
+ * Remove from databse
+ */
+router.post("/remove", authenticate, (req, res) => {
+
+    if (!req.body.submissionName) {
+        res.status(400).json({ message: "No submission data" });
+        return;
+    }
+
+    User.findOneAndRemove({ username: req.user.username, submissionName: req.body.submissionName }).then(() => {
+            res.status(200).send({ message: "Submission succesfully removed!" })
+        }).catch((err) => {
+            res.status(400).send({ message: "No submission found" });
+            res.send(err);
+        })
+})
+
 router.get("/mine", authenticate, (req, res) => {
-    Submission.find({username: req.user.username}).then((subs) => {
+    Submission.find({ username: req.user.username }).then((subs) => {
         res.send(subs);
     });
 });
 
 router.get("/all", authenticate, (req, res) => {
 
-    if (req.user.status == 'admin'){
+    if (req.user.status == 'admin') {
         Submission.find({}).then((subs) => {
             res.send(subs);
         });
     }
-    else{
-        res.status(401).send({message: '401 ERROR: Access Denied'})
+    else {
+        res.status(401).send({ message: '401 ERROR: Access Denied' })
     }
 });
 
