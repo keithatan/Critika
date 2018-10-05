aa8c7dc25243fe6f2a2b6a3bb55475ffab80e53a
Cyrus Santiago <santiac@purdue.edu>
2018-10-05 02:20:15 -0500

    added carousels to view submissions on dashboard page

--- frontend/critika/src/Dashboard.js
+++ frontend/critika/src/Dashboard.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './App.css'
-import {Button, Icon} from 'antd'
+import {Button, Icon, Carousel} from 'antd'
 import {Link} from 'react-router-dom'
+import thumbnail from './default image.jpg'
+
 
 class Dashboard extends React.Component{
   render(){
+    function onChange(a, b, c) {
+      console.log(a, b, c);
+    }
   return (
     <div>
         <Button.Group size={"small"}>
@@ -15,6 +20,36 @@ class Dashboard extends React.Component{
  
       <h1>Dashboard</h1>
       {/* <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)'}} />} placeholder="Username" /> */}
+      <div>Your Queue</div>
+      <Carousel afterChange={onChange} style={{backgroundColor: 'rgb(194, 165, 211)'}}>
+        <div>
+          <Button style={{  height: '7em', borderRadius: '0.9em' }} type="flex" justify="center"><img style={{ height: '7em', borderRadius: '0.9em' }} src={thumbnail}/></Button>
+        </div>
+        <div>      
+        <Button style={{  height: '7em', borderRadius: '0.9em' }} type="flex" justify="center"><img style={{ height: '7em', borderRadius: '0.9em' }} src={thumbnail}/></Button>
+        </div>
+        <div>      
+        <Button style={{  height: '7em', borderRadius: '0.9em' }} type="flex" justify="center"><img style={{ height: '7em', borderRadius: '0.9em' }} src={thumbnail}/></Button>
+        </div>
+        <div>      
+        <Button style={{  height: '7em', borderRadius: '0.9em' }} type="flex" justify="center"><img style={{ height: '7em', borderRadius: '0.9em' }} src={thumbnail}/></Button>
+        </div>
+      </Carousel>
+      <div>Popular Submissions</div>
+      <Carousel afterChange={onChange} style={{backgroundColor: 'rgb(194, 165, 211)'}}>
+        <div>
+          <Button style={{  height: '7em', borderRadius: '0.9em' }} type="flex" justify="center"><img style={{ height: '7em', borderRadius: '0.9em' }} src={thumbnail}/></Button>
+        </div>
+        <div>      
+        <Button style={{  height: '7em', borderRadius: '0.9em' }} type="flex" justify="center"><img style={{ height: '7em', borderRadius: '0.9em' }} src={thumbnail}/></Button>
+        </div>
+        <div>      
+        <Button style={{  height: '7em', borderRadius: '0.9em' }} type="flex" justify="center"><img style={{ height: '7em', borderRadius: '0.9em' }} src={thumbnail}/></Button>
+        </div>
+        <div>      
+        <Button style={{  height: '7em', borderRadius: '0.9em' }} type="flex" justify="center"><img style={{ height: '7em', borderRadius: '0.9em' }} src={thumbnail}/></Button>
+        </div>
+      </Carousel>
 
 
      
