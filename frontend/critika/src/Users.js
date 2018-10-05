70c2fccf6aacab2469b40f9b7b18087ce5af16f0
Cyrus Santiago <santiac@purdue.edu>
2018-10-05 15:00:50 -0500

    added options menu on uses page

--- frontend/critika/src/Users.js
+++ frontend/critika/src/Users.js
@@ -19,6 +19,19 @@ class Users extends React.Component{
           </Menu.Item>
         </Menu>
       );
+      const optionMenu = (
+        <Menu>
+          <Menu.Item>
+            <a>Ban</a>
+          </Menu.Item>
+          <Menu.Item>
+            <a>Suspend</a>
+          </Menu.Item>
+          <Menu.Item>
+            <a>Message</a>
+          </Menu.Item>
+        </Menu>
+      );
   return (
     <div>
       <h1>Users</h1>
@@ -33,49 +46,81 @@ class Users extends React.Component{
       <Col span={12}>
         <Card title="User1" bordered={false} style={{borderRadius: '0.9em' }}>
         <p><img style={{ height: '5em', borderRadius: '0.9em' }} src={profilePic}/>User 1</p>
-        
+        <Dropdown overlay={optionMenu}>
+        <a className="ant-dropdown-link" href="#">
+          Actions <Icon type="down" />
+        </a>
+      </Dropdown>
         
         </Card>
         <p></p>
         <Card title="User3" bordered={false} style={{borderRadius: '0.9em' }}>
         <p><img style={{ height: '5em', borderRadius: '0.9em' }} src={profilePic}/>User 3</p>
-        
+        <Dropdown overlay={optionMenu}>
+        <a className="ant-dropdown-link" href="#">
+          Actions <Icon type="down" />
+        </a>
+      </Dropdown>
         </Card>
         <p></p>
 
         <Card title="User5" bordered={false} style={{borderRadius: '0.9em' }}>
         <p><img style={{ height: '5em', borderRadius: '0.9em' }} src={profilePic}/>User 5</p>
-        
+        <Dropdown overlay={optionMenu}>
+        <a className="ant-dropdown-link" href="#">
+          Actions <Icon type="down" />
+        </a>
+      </Dropdown>
         </Card>
         <p></p>
 
         <Card title="User7" bordered={false} style={{borderRadius: '0.9em' }}>
         <p><img style={{ height: '5em', borderRadius: '0.9em' }} src={profilePic}/>User 7</p>
-        
+        <Dropdown overlay={optionMenu}>
+        <a className="ant-dropdown-link" href="#">
+          Actions <Icon type="down" />
+        </a>
+      </Dropdown>
         </Card>
       </Col>
       <Col span={12}>
         <Card title="User2" bordered={false} style={{borderRadius: '0.9em' }}>
         <p><img style={{ height: '5em', borderRadius: '0.9em' }} src={profilePic}/>User 2</p>
-        
+        <Dropdown overlay={optionMenu}>
+        <a className="ant-dropdown-link" href="#">
+          Actions <Icon type="down" />
+        </a>
+      </Dropdown>
         </Card>
         <p></p>
 
         <Card title="User4" bordered={false} style={{borderRadius: '0.9em' }}>
         <p><img style={{ height: '5em', borderRadius: '0.9em' }} src={profilePic}/>User 4</p>
-        
+        <Dropdown overlay={optionMenu}>
+        <a className="ant-dropdown-link" href="#">
+          Actions <Icon type="down" />
+        </a>
+      </Dropdown>
         </Card>
         <p></p>
 
         <Card title="User6" bordered={false} style={{borderRadius: '0.9em' }}>
         <p><img style={{ height: '5em', borderRadius: '0.9em' }} src={profilePic}/>User 6</p>
-        
+        <Dropdown overlay={optionMenu}>
+        <a className="ant-dropdown-link" href="#">
+          Actions <Icon type="down" />
+        </a>
+      </Dropdown>
         </Card>
         <p></p>
 
         <Card title="User8" bordered={false} style={{borderRadius: '0.9em' }}>
         <p><img style={{ height: '5em', borderRadius: '0.9em' }} src={profilePic}/>User 8</p>
-        
+        <Dropdown overlay={optionMenu}>
+        <a className="ant-dropdown-link" href="#">
+          Actions <Icon type="down" />
+        </a>
+      </Dropdown>
         </Card>
       </Col>
      
