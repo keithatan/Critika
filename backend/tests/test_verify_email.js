295cce222d902324f6533c0790870011ef052be8
Connor Todd <todd37@purdue.edu>
2018-10-05 04:59:15 -0400

    Add new unit test for allUsers route, add command to script

---
+++ backend/tests/test_verify_email.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../app');
+var should = chai.should();
+
+/* USAGE mocha /tests/<file> */
+
+
+chai.use(chaiHttp);
+
+describe('Test verify email', function(){
+
+    
+
+})
