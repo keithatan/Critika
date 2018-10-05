09f26d79a0f952ada575f740a0a33a6f05c506ec
Connor Todd <todd37@purdue.edu>
2018-10-05 05:25:42 -0400

    More unit test and scripts

--- backend/runtest.sh
+++ backend/runtest.sh
@@ -7,3 +7,4 @@ mocha tests/test_submission_mine.js --exit
 mocha tests/test_user_account.js --exit
 mocha tests/test_verify_email.js --exit
 mocha tests/test_getAllUsers --exit
+mocha tests/test_editInfo --exit
