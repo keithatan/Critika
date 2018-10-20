3f952e816a6ee250a2b394ce8bfdcdd04bd7fada
Connor Todd <todd37@purdue.edu>
2018-10-19 23:58:55 -0400

    Make create community route. Founder gets added as a moderator, database is updated correctly, also /community routes are now functional and dont hang

--- model/community.js
+++ model/community.js
@@ -4,17 +4,18 @@ let Schema = mongoose.Schema;
 /* Make the schema */
 let communitySchema = new Schema({
     communityName: {type: String, required: true},
-    communityDescription: {type: String},
-    communityID: {type: String},
+    communityDescription: {type: String, required: true},
     dateCreated: {type: Date, default: Date.now},
-    numberOfSubscribers: {type: Number, default: 0},
+    founder: {type: String},
+    numberOfSubscribers: {type: Number, default: 1},
     moderators: {type: [String]},
-
     /* Need to store all the submissions for this community, Array? of what? */
   });
   
   
   /* Creating the user model from the schema and giving it to Mongoose */
   let community = mongoose.model('Community', communitySchema);
+
+  
   
   module.exports = community;
