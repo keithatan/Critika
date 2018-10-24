af222e937da8703ec211486f802ba6a05ad0b7bd
kcsodetz <kensodetz@gmail.com>
2018-10-24 11:44:57 -0400

    Working Login functionality

--- frontend/critika/src/Login.js
+++ frontend/critika/src/Login.js
@@ -3,12 +3,17 @@ import './App.css'
 import axios from 'axios'
 import { Form, Icon, Input, Button, Checkbox, Col } from 'antd';
 import { Link } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 import googlebutton from './login with google.png'
 import githubbutton from './login with github.png'
 
 const FormItem = Form.Item;
 
 class Login extends React.Component {
+
+  state = {
+    redirect: false
+  }
 
   sendLogin = async (userName, password) => {
     try {
@@ -25,8 +30,10 @@ class Login extends React.Component {
     e.preventDefault();
     this.props.form.validateFields(async (err, values) => {
       if (!err) {
-        console.log('Received values of form: ', values);
         const response = await this.sendLogin(values.userName, values.password)
+        console.log(response)
+        if (response != undefined)
+          this.setState({ redirect: true })
       }
       else {
         console.log(err)
@@ -35,41 +42,47 @@ class Login extends React.Component {
   }
 
   render() {
+
+    const { redirect } = this.state;
+    if (redirect) {
+      return <Redirect to='/dashboard' />
+    }
+
     const { getFieldDecorator } = this.props.form;
     return (
       <Col span={8} offset={8}>
-      <Form style={{ width: "300px", textAlign: "center" }} onSubmit={this.handleSubmit} className="login-form">
-        <FormItem>
-          {getFieldDecorator('userName', {
-            rules: [{ required: true, message: 'Please input your username!' }],
-          })(
-            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
-          )}
-        </FormItem>
-        <FormItem>
-          {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Please input your Password!' }],
-          })(
-            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-          )}
-        </FormItem>
+        <Form style={{ width: "300px", textAlign: "center" }} onSubmit={this.handleSubmit} className="login-form">
+          <FormItem>
+            {getFieldDecorator('userName', {
+              rules: [{ required: true, message: 'Please input your username!' }],
+            })(
+              <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+            )}
+          </FormItem>
+          <FormItem>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: 'Please input your Password!' }],
+            })(
+              <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+            )}
+          </FormItem>
 
-        <FormItem>
-          <Checkbox>Remember me</Checkbox>
-          <br></br>
-          {/* <a className="login-form-forgot" href="">Forgot password</a> */}
-          <p>
-            <Button type="primary" htmlType="submit" className="login-form-button">
-              Log in     
+          <FormItem>
+            <Checkbox>Remember me</Checkbox>
+            <br></br>
+            {/* <a className="login-form-forgot" href="">Forgot password</a> */}
+            <p>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                Log in
              </Button>
-            Or  
+              Or
              <Link to="/register">Register</Link>
-            <h4>Forgot Password?</h4>
-            <Link className="login-form-forgot" to="/resetPassword">Reset Password</Link>
-            {/* <a href="">register now!</a> */}
-          </p>
-        </FormItem>
-      </Form>
+              <h4>Forgot Password?</h4>
+              <Link className="login-form-forgot" to="/resetPassword">Reset Password</Link>
+              {/* <a href="">register now!</a> */}
+            </p>
+          </FormItem>
+        </Form>
       </Col>
     );
   }
