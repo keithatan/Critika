b68d5ce9b708fcf552922a2e0756312a4ea59840
Connor Todd <todd37@purdue.edu>
2018-10-31 20:55:28 -0400

    Remove redundency

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -436,37 +436,4 @@ router.post("/reset-password-email", (req, res) => {
     }
 })
 
-// /**
-//  * Sends an email from critika.app@gmail.com to the specified 'to' email, with a subject and body given by the 
-//  * function caller.
-//  * @param {string} to 
-//  * @param {string} subject 
-//  * @param {string} body 
-//  */
-// function sendEmail(to, subject, body) {
-//     var transporter = nodemailer.createTransport({
-//         service: 'gmail',
-//         auth: {
-//             user: email_address,
-//             pass: process.env.EMAIL_PASSWD
-//         }
-//     });
-
-//     var mailOptions = {
-//         from: email_address,
-//         to: to,
-//         subject: subject,
-//         text: body
-//     };
-
-//     console.log("Sending to: " + to)
-//     transporter.sendMail(mailOptions, function (error, info) {
-//         if (error) {
-//             console.error(error);
-//         } else {
-//             console.log('Email sent: ' + info.response);
-//         }
-//     });
-// }
-
 module.exports = router;
