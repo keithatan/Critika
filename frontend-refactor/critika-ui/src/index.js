0fe0d7a1db72e7d8f958ec4d64130600b031b948
keithatan <tan230@purdue.edu>
2018-11-01 02:16:01 -0400

    Made action for register & allowed x-auth to be retrieved as response header

--- frontend-refactor/critika-ui/src/index.js
+++ frontend-refactor/critika-ui/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider } from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 import './index.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import promise from 'redux-promise';
@@ -11,10 +12,13 @@ import Home from './components/Home/Home';
 import Test from './components/Test/test';
 import SignupPage from './components/SignupPage/SignupPage';
 
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+const store = createStore(
+    (state ={}) => state,
+    applyMiddleware(thunk)
+);
 
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
+    <Provider store={store}>
         <BrowserRouter>
             <div>
                 <Route path="/" component={ Home }/>
