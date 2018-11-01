3fc036c3ada653aacf21103576136f0067942f62
Connor Todd <todd37@purdue.edu>
2018-10-31 20:57:53 -0400

    Add email to .env

--- middleware/email.js
+++ middleware/email.js
@@ -1,6 +1,6 @@
 var nodemailer = require('nodemailer');
 
-var email_address = "critika.app@gmail.com";
+var email_address = process.env.EMAIL;
 
 /**
  * Sends an email from critika.app@gmail.com to the specified 'to' email, with a subject and body given by the 
