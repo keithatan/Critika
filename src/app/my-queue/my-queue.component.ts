ec0a9c1cb7054df088394c06026cf129f683edb0
keithatan <tan230@purdue.edu>
2018-11-01 21:02:05 -0400

    Created dashboard

---
+++ src/app/my-queue/my-queue.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-queue',
+  templateUrl: './my-queue.component.html',
+  styleUrls: ['./my-queue.component.scss']
+})
+export class MyQueueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
