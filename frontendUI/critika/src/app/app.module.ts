db2666e4ba9dc34c3f2a33409757ca5ed8f408fd
keithatan <tan230@purdue.edu>
2018-11-01 21:02:05 -0400

    Created dashboard

--- frontendUI/critika/src/app/app.module.ts
+++ frontendUI/critika/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { RouterModule } from '@angular/router';
+import { AppRoutes } from './app.routing';
 
-import { AppRoutingModule } from './app-routing.module';
+
+//import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { MyQueueComponent } from './my-queue/my-queue.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HomeComponent,
+    MyQueueComponent
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot(),
-    AppRoutingModule
+    RouterModule.forRoot(AppRoutes),
+    MDBBootstrapModule.forRoot()
+    //AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontendUI/critika/src/app/app.routing.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+export const AppRoutes: Routes = [
+    {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+]
