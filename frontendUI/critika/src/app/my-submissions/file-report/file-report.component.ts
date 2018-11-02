c78856f1a1f89fff764ec20a2f4bdeb9d62f5ab0
julzsd12 <julzsd12@gmail.com>
2018-11-02 06:47:11 -0400

    ban user again

---
+++ frontendUI/critika/src/app/my-submissions/file-report/file-report.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-file-report',
+  templateUrl: './file-report.component.html',
+  styleUrls: ['./file-report.component.scss']
+})
+export class FileReportComponent implements OnInit {
+
+  constructor() { 
+  }
+
+  ngOnInit() {
+  }
+
+}
