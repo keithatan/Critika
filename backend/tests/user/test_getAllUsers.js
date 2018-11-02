d62aa0e529244c70ca862c0719300a46f9ced234
Connor Todd <todd37@purdue.edu>
2018-11-02 01:02:51 -0400

    Add tests for two more routes

--- backend/tests/user/test_getAllUsers.js
+++ backend/tests/user/test_getAllUsers.js
@@ -5,9 +5,9 @@ var server = require('../../app');
 var should = chai.should();
 var functions = require('../unitTestFunctions.js')
 
-var Submission = require('../model/submission');
-var User = require('../model/user');
-var Community = require('../model/community')
+var Submission = require('../../model/submission');
+var User = require('../../model/user');
+var Community = require('../../model/community')
 
 var uname = process.env.UNIT_TEST_USERNAME
 var pword = process.env.UNIT_TEST_PASSWORD
@@ -17,22 +17,88 @@ chai.use(chaiHttp);
 
 describe('Test GET allUsers', () => {
 
-    before(function(done){
-        User.remove({username: uname}, function(err){
-            console.log(err)
-        })
-        done();
-    })
-
     describe('User is not an admin', () =>{
-
+        it('Should return 400', function(done) {
+            var info = {
+                username: uname,
+                password: pword,
+            }
+        
+            User.findOne({username: uname}, (err, user) => {
+            chai.request(server)
+            .post('/user/login')
+            .set('content-type', 'application/x-www-form-urlencoded')
+            .send(info)
+            .end((err, res) => {
+                //do the get request here 
+                chai.request(server)
+                .get('/user/allUsers')
+                .set('content-type', 'application/x-www-form-urlencoded')
+                .set('token', res.header['token'])
+                .
+                end((err, res) => {
+                    res.should.have.status(400)
+                })
+                })
+            });
+            done()
+        })
     })
 
-    describe('User has bad auth', () =>{
+    describe('User has bad auth', () => {
+        User.findOneAndUpdate({username: uname}, {$set: {status: 'admin'}})
 
+        it('Should return 400', function(done) {
+            var info = {
+                username: uname,
+                password: pword,
+            }
+        
+            User.findOne({username: uname}, (err, user) => {
+            chai.request(server)
+            .post('/user/login')
+            .set('content-type', 'application/x-www-form-urlencoded')
+            .send(info)
+            .end((err, res) => {
+                //do the get request here 
+                chai.request(server)
+                .get('/user/allUsers')
+                .set('content-type', 'application/x-www-form-urlencoded')
+                .set('token', 'bad token')
+                .end((err, res) => {
+                    res.should.have.status(400)
+                })
+                })
+            });
+            done()
+        })
     })
 
     describe('Correct info', () =>{
-
+        it('Should return 200', function(done) {
+            var info = {
+                username: uname,
+                password: pword,
+            }
+        
+            User.findOne({username: uname}, (err, user) => {
+            chai.request(server)
+            .post('/user/login')
+            .set('content-type', 'application/x-www-form-urlencoded')
+            .send(info)
+            .end((err, res) => {
+                //do the get request here 
+                chai.request(server)
+                .get('/user/allUsers')
+                .set('content-type', 'application/x-www-form-urlencoded')
+                .set('token', res.header['token'])
+                .
+                end((err, res) => {
+                    res.should.have.status(400)
+                })
+                })
+            });
+            done()
+        })
     })
 })
