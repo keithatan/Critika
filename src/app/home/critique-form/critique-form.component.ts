1668466c030ba4b52ad8ba4f68fa0694c8ab8450
julzsd12 <julzsd12@gmail.com>
2018-11-02 04:53:15 -0400

    made a form for critiquing

---
+++ src/app/home/critique-form/critique-form.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-critique-form',
+  templateUrl: './critique-form.component.html',
+  styleUrls: ['./critique-form.component.scss']
+})
+export class CritiqueFormComponent implements OnInit {
+
+  constructor() { 
+    this.renderComponent = "";
+  }
+  renderComponent: String;
+
+  ngOnInit() {
+  }
+
+}
