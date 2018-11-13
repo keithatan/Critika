12e6d93b94418b5f24a562e523d1de47b2e0e7bc
Connor Todd <todd37@purdue.edu>
2018-11-12 20:23:38 -0500

    Refactor, edit community routes, attempt to link service and component

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -5,20 +5,43 @@ import { Category } from './categories.model';
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
+  styleUrls: ['./categories.component.scss'],
+  providers: [CategoriesService]
 })
+
 export class CategoriesComponent implements OnInit {
-    allCategories:Category[];
-    renderComponent: string = "";
-  constructor(public subService:CategoriesService) { }
-  getAllCommunitiesPls(){
-    this.allCategories = this.subService.getAllCategories();
+  allCategories: Category[];
+  categoriesForFilter: Category[];
+  renderComponent: string = "";
+  categoryNames: String[];
+
+  constructor(private subService: CategoriesService) { 
+    this.allCategories = subService.getAllCategories();
+    console.log(subService)
+    console.log(subService.allCategoriesInService)
   }
+
+  getAllCategories() {
+    // console.log(3)
+    console.log(this.allCategories)
+    // this.allCategories = this.subService.getAllCategories();
+  }
+
   renderFileReport() {
-        this.renderComponent = "file-report";
+    console.log(2)
+    this.renderComponent = "file-report";
   }
 
   ngOnInit() {
+    console.log(this.subService)
+  }
+
+  onSearchChange(searchValue: string) {
+    let i: number;
+    // for (i = 0; i < this.allCategories.length; i += 1) {
+    //   console.log(this.allCategories[i])
+    // }
+    console.log(this.allCategories)
   }
 
 }
