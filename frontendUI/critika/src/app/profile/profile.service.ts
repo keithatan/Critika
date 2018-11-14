d7a2776c8e2b2e8671b9fcfc5bf92472811d9f2b
julzsd12 <julzsd12@gmail.com>
2018-11-13 21:56:41 -0500

    implemented getProfile

--- frontendUI/critika/src/app/profile/profile.service.ts
+++ frontendUI/critika/src/app/profile/profile.service.ts
@@ -14,6 +14,13 @@ export class ProfileService {
 
     }
 
+    getProfile(){
+        //make API call, then call toPromise
+        // calling the api and then letting X know that im done and heres the information
+        return this.http.get("http://localhost:5000/user/account").toPromise()
+
+    }
+
     editProfile(username:string, membersince: Date, rating: Number, coins: Number, aboutme: string){
         const profile:Object = {
                 username: username,
