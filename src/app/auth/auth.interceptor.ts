b8ea6f1c8367a072e45c54459517bb2ba3eed1bc
kcsodetz <kensodetz@gmail.com>
2018-11-14 22:58:38 -0500

    Complete validation for login

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -3,32 +3,32 @@ import { Injectable } from "@angular/core";
 import { AuthService } from "./auth.service";
 
 @Injectable()
-export class AuthInterceptor implements HttpInterceptor{
-    constructor(private authService: AuthService){
+export class AuthInterceptor implements HttpInterceptor {
+    constructor(private authService: AuthService) {
 
     }
-    intercept(req:HttpRequest<any>, next:HttpHandler){
+    intercept(req: HttpRequest<any>, next: HttpHandler) {
         const authToken = localStorage.getItem('token')
         //this.authService.getToken();
         // console.log("Intercept")
-        if (authToken){
-        const authRequest = req.clone({
+        if (authToken) {
+            const authRequest = req.clone({
 
-            headers: req.headers.set('token', authToken)
-        
+                headers: req.headers.set('token', authToken)
 
-        });
-        // console.log("1")
-        return next.handle(authRequest)
 
-        }else{
+            });
+            // console.log("1")
+            return next.handle(authRequest)
+
+        } else {
             const authRequest = req.clone();
             // console.log("2")
 
             return next.handle(authRequest)
 
         }
-        
-        
+
+
     }
 }
