eff78eef4ae5c0db969575b69781ecce74c9ab28
keithatan <tan230@purdue.edu>
2018-11-24 00:20:59 -0600

    Added critiquing logic to service

--- src/app/models/feedback.model.ts
+++ src/app/models/feedback.model.ts
@@ -3,9 +3,11 @@ export class Feedback {
     anonymous: boolean;
     feedbackMessage: string;
     feedbackSubject: string;
+    submissionID: string;
     feedbackRating: Number;
 
     constructor(response: any){
+        this.submissionID = response.submissionID;
         this.username = response.username;
         this.anonymous = response.anonymous;
         this.feedbackMessage = response.feedbackMessage;
