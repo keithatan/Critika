44c7cac47f4a1d8728e1df950ace52ce2263e69f
keithatan <tan230@purdue.edu>
2018-11-29 03:48:06 -0500

    View other user

--- frontendUI/critika/src/app/services/profile.service.ts
+++ frontendUI/critika/src/app/services/profile.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient , HttpHeaders} from "@angular/common/http";
+import { HttpClient , HttpHeaders, HttpParams} from "@angular/common/http";
 import { Subject, Observable } from "rxjs";
 import { Profile } from '../models/profile.model'; 
 
@@ -34,5 +34,12 @@ export class ProfileService {
 
     getAllUsers(){
         return this.http.get("http://localhost:5000/user/all-users").toPromise();
+    }
+
+    getUser(user:string){
+        const param:Object = {
+            username: user
+        }
+        return this.http.post("http://localhost:5000/user/find", param);
     }
 }
