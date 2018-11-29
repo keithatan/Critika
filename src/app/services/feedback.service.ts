652f0f6aaf3eaab510ea3bd8d0eb6e1bf9f83e1a
julzsd12 <julzsd12@gmail.com>
2018-11-29 03:51:04 -0500

    added rating system to let user to rate feedback

--- src/app/services/feedback.service.ts
+++ src/app/services/feedback.service.ts
@@ -10,20 +10,29 @@ export class FeedbackService {
     feedbackReceived: Feedback[];
     data: Object;
     constructor(private http:HttpClient){ }
-
+    
     //rate feedback received
-   rateFeedback (feedbackmessage: string, feedbacksubject: string, feedbackrating: Number, 
-                username: string, anonymous: boolean){
-       const feedback:Object = {feedbackMessage: feedbackmessage, feedbackSubject: feedbacksubject,
-                                 feedbackRating: feedbackrating, username: username, anonymous: anonymous }
-
-        var p = new Feedback(feedback);
-        this.http.post("http://localhost:5000/feedback/rate", p)
-        .subscribe(response => {
-            console.log(response)
-            
-        });
-   }
+    //    rateFeedback (feedbackmessage: string, feedbacksubject: string, feedbackrating: Number, 
+    //                 username: string, anonymous: boolean){
+    //        const feedback:Object = {feedbackMessage: feedbackmessage, feedbackSubject: feedbacksubject,
+    //                                  feedbackRating: feedbackrating, username: username, anonymous: anonymous }
+    
+    //         var p = new Feedback(feedback);
+    //         this.http.post("http://localhost:5000/feedback/rate", p)
+    //         .subscribe(response => {
+    //             console.log(response)
+    
+    //         });
+    //    }
+    
+    rateFeedback (feedbackrating: Number, submissionid: string){
+        const feedback:Object = {
+            feedbackRating: feedbackrating,
+            submissionID: submissionid
+        }
+        
+        return this.http.post("http://localhost:5000/feedback/rate-feedback", feedback)
+    }
    
    giveFeedback(feedbackbad: string, feedbackwork: string, feedbackgood: string,
     username: string, submissionName:string, anonymous:boolean, submissionID: string){
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -9,29 +9,29 @@ import { Profile } from '../models/profile.model';
 
 export class ProfileService {
     renderComponent: String = "";
-
+    
     constructor(private http:HttpClient) {
-
+        
     }
-
+    
     getProfile(){
         //make API call, then call toPromise
         // calling the api and then letting X know that im done and heres the information
         return this.http.get("http://localhost:5000/user/account").toPromise()
-
+        
     }
-
+    
     editProfile(homepage:string, loc: string, occupation: string, aboutme: string){
         const profile:Object = {
-                location: loc,
-                homepage: homepage,
-                occupation: occupation,
-                aboutme: aboutme
+            location: loc,
+            homepage: homepage,
+            occupation: occupation,
+            aboutme: aboutme
         }
-
+        
         return this.http.post("http://localhost:5000/user/profile", profile)
     }
-
+    
     getAllUsers(){
         return this.http.get("http://localhost:5000/user/all-users").toPromise();
     }
