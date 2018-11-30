f0fda8024ccfad9ca66372bd9ec3de35b6ae6b6d
keithatan <tan230@purdue.edu>
2018-11-30 07:11:26 -0500

    Added animations to renders

---
+++ src/app/usersubmission/usersubmission.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usersubmission',
+  templateUrl: './usersubmission.component.html',
+  styleUrls: ['./usersubmission.component.scss']
+})
+export class UsersubmissionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
