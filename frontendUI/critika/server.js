62be0aac3e8e6eefc73cc513b1a245b1e1a7a717
keithatan <tan230@purdue.edu>
2018-11-30 11:12:47 -0500

    fix

--- frontendUI/critika/server.js
+++ frontendUI/critika/server.js
@@ -5,7 +5,7 @@ app.use(express.static(__dirname +'/dist'));
 
 app.all('*',(req,res) =>{
 
-    res.status(200).sendFile(__dirname + '/dist/index.html')
+    res.status(200).sendFile(__dirname + '/index.html')
 })
 
 app.listen(process.env.PORT || 8080)
