ebc99ea64b720011f65f9a5467c0aac9dc5df2d8
kcsodetz <kensodetz@gmail.com>
2018-11-30 10:45:47 -0500

    Add verification to comments submission

--- frontendUI/critika/src/app/my-submissions/view-sub/view-sub.component.ts
+++ frontendUI/critika/src/app/my-submissions/view-sub/view-sub.component.ts
@@ -8,54 +8,59 @@ import { SubmissionService } from '../../services/submissions.service';
 })
 export class ViewSubComponent implements OnInit {
   tableElements = ['User', 'Comment', 'Report'];
-  @Input('childSubmission') sub:Submission;
+  @Input('childSubmission') sub: Submission;
   @Output() returnToParent = new EventEmitter<string>();
   chosenSubmission: Submission;
   renderComponent: string;
   comment: string = "";
   response: string = "NULL";
   edit: boolean;
+  commentBlank:boolean = false;
 
-  constructor(public subService:SubmissionService) { 
+  constructor(public subService: SubmissionService) {
     this.renderComponent = "";
     this.edit = false;
   }
 
   get response_msg() { return this.response; }
+  get is_comment_blank() { return this.commentBlank; }
 
-  sendReport(submission:Submission, commentMessage:string, comment){
+  sendReport(submission: Submission, commentMessage: string, comment) {
     // console.log(sub)
     this.subService.reportComment(comment._id, submission.submissionID, commentMessage).then((res) => {
       this.response = res["message"];
     });
   }
 
-  renderEditSub(sub:Submission) {
-   
-      this.renderComponent = 'EditSub';
-      this.chosenSubmission = sub;
-  
-}
+  renderEditSub(sub: Submission) {
 
-addComment(sub){
-  console.log(sub)
-  if (this.comment == "") {
-    return;
+    this.renderComponent = 'EditSub';
+    this.chosenSubmission = sub;
+
   }
-  this.subService.addComment(this.comment, sub.submissionID).then((sub) => {
-    console.log(sub)
-  })
-  this.returnToParent.emit('reload');
-}
 
-getChildEvent(event:string){
-  this.returnToParent.emit('reload');
-}
+  addComment(sub) {
+    console.log(sub.submissionID)
 
-close(){
-  this.returnToParent.emit('reload');
+    if (this.comment == "") {
+      this.commentBlank = true;
+      return;
+    }
+    this.commentBlank = false;
+    this.subService.addComment(this.comment, sub.submissionID).then((sub) => {
+      console.log(sub)
+    })
+    this.returnToParent.emit('reload');
+  }
 
-}
+  getChildEvent(event: string) {
+    this.returnToParent.emit('reload');
+  }
+
+  close() {
+    this.returnToParent.emit('reload');
+
+  }
 
   ngOnInit() {
     console.log(this.sub)
